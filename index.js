6f4cfa415cd8104810077a9918731b6858503230
João Rodrigues <joao.nrr@gmail.com>
2023-08-30 11:00:12 -0300

    Criada função assíncrona main()

--- index.js
+++ index.js
@@ -1,71 +1,82 @@
 const express = require("express");
-const app = express();
+const { MongoClient } = require("mongodb");
 
-// Habilitamos o processamento de JSON
-app.use(express.json());
+const url = "mongodb://localhost:27017";
+const db = "jornada-backend";
+const client = new MongoClient(url);
 
-// Endpoint Principal
-app.get("/", function (req, res) {
-    res.send("Hello World");
-});
+async function main() {
 
-// Endpoint /oi
-app.get("/oi", function (req, res) {
-    res.send("Olá, mundo!");
-});
+    const app = express();
 
-// Endpoints de Herois
-const lista = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
-//             0                    1                2
+    // Habilitamos o processamento de JSON
+    app.use(express.json());
 
-// Read All -> [GET] /herois
-app.get("/herois", function (req, res) {
-    res.send(lista.filter(Boolean));
-});
+    // Endpoint Principal
+    app.get("/", function (req, res) {
+        res.send("Hello World");
+    });
 
-// Create -> [POST] /herois
-app.post("/herois", function (req, res) {
-    // console.log(req.body, typeof req.body);
-    
-    const item = req.body.nome;
-    lista.push(item)
-    res.send("Item criado com sucesso!")
+    // Endpoint /oi
+    app.get("/oi", function (req, res) {
+        res.send("Olá, mundo!");
+    });
 
-});
+    // Endpoints de Herois
+    const lista = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
+    //             0                    1                2
 
-// Read by ID -> GET /herois/:id
-// usamos ":" no express
+    // Read All -> [GET] /herois
+    app.get("/herois", function (req, res) {
+        res.send(lista.filter(Boolean));
+    });
 
-app.get("/herois/:id", function(req, res) {
-    // parametro de rota
-    // Pegar o parâmetro e salvar na const id
-    const id = req.params.id - 1;
+    // Create -> [POST] /herois
+    app.post("/herois", function (req, res) {
+        // console.log(req.body, typeof req.body);
+        
+        const item = req.body.nome;
+        lista.push(item)
+        res.send("Item criado com sucesso!")
 
-    // Pegar a informação da lista
-    const item = lista[id];
+    });
 
-    res.send("ID recebido: " + id + "\nCorresponde ao item: " + item)
-});
+    // Read by ID -> GET /herois/:id
+    // usamos ":" no express
 
+    app.get("/herois/:id", function(req, res) {
+        // parametro de rota
+        // Pegar o parâmetro e salvar na const id
+        const id = req.params.id - 1;
 
-// Update -> [PUT] /herois/:id
-app.put("/herois/:id", function (req, res) {
-    const id = req.params.id - 1;
-    const item = req.body.nome;
+        // Pegar a informação da lista
+        const item = lista[id];
 
-    lista[id] = item
-
-    res.send("Item editado com sucesso")
-});
+        res.send("ID recebido: " + id + "\nCorresponde ao item: " + item)
+    });
 
 
-// Delete -> [DELETE] /herois/:id
-app.delete("/herois/:id", function (req, res) {
-    const id = req.params.id - 1;
+    // Update -> [PUT] /herois/:id
+    app.put("/herois/:id", function (req, res) {
+        const id = req.params.id - 1;
+        const item = req.body.nome;
 
-    delete lista[id];
+        lista[id] = item
 
-    res.send("Item excluido com sucesso")
-})
+        res.send("Item editado com sucesso")
+    });
 
-app.listen(3000);
+
+    // Delete -> [DELETE] /herois/:id
+    app.delete("/herois/:id", function (req, res) {
+        const id = req.params.id - 1;
+
+        delete lista[id];
+
+        res.send("Item excluido com sucesso")
+    })
+
+    app.listen(3000);
+}
+
+main();
